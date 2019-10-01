e6755b9fe17041d4c0a5dc3796b9a9907d94a9b0
John Reiley <supermormonbrother@gmail.com>
2019-10-01 15:35:55 -0600

    test

--- web/week03/team/welcome.php
+++ web/week03/team/welcome.php
@@ -3,23 +3,26 @@
 <link rel="stylesheet" href="style.css">
 
 <body>
-    <div>Welcome <?php echo $_POST["firstname"]; ?></div> <br>
-    <div>Your email address is: <?php echo $_POST["email"]; ?></div>
-    <a href="mailto:<?php echo $_POST["email"] ?>" target="_top">Send Mail to Self</a>
+    <div class="container">
+        <div>Welcome <?php echo $_POST["firstname"]; ?></div> <br>
+        <div>Your email address is: <?php echo $_POST["email"]; ?></div>
+        <a href="mailto:<?php echo $_POST["email"] ?>" target="_top">Send Mail to Self</a>
 
-    <div> You are majoring in <?php echo $_POST["major"] ?> </div>
-    <div>You said "<?php echo $_POST["comments"] ?>"</div>
+        <div> You are majoring in <?php echo $_POST["major"] ?> </div>
+        <div>You said "<?php echo $_POST["comments"] ?>"</div>
 
-    <div>
-        <p>You have visited:</p>
-        <ul>
-            <?php
-            foreach ($_POST['places'] as $place) {
-                echo "<li> $place </li>";
-            }
-            ?>
-        </ul>
-        <a href="form.html">Back To Form</a>
+        <div>
+            <p>You have visited:</p>
+            <ul>
+                <?php
+                echo var_dump($_POST['places']);
+                foreach ($_POST['places'] as $place) {
+                    echo "<li> $place </li>";
+                }
+                ?>
+            </ul>
+            <a href="form.html">Back To Form</a>
+        </div>
     </div>
 </body>
 
