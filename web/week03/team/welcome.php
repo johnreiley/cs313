c7ed70f54deaf99e8d5351841fcb1025e33d7a8b
John Reiley <supermormonbrother@gmail.com>
2019-10-01 15:27:39 -0600

    test

--- web/week03/team/welcome.php
+++ web/week03/team/welcome.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" href="style.css">
+
 <body>
     <div>Welcome <?php echo $_POST["firstname"]; ?></div> <br>
     <div>Your email address is: <?php echo $_POST["email"]; ?></div>
-    <a href="mailto:<?php echo $_POST["email"]?>" target="_top">Send Mail to Self</a>
-    
+    <a href="mailto:<?php echo $_POST["email"] ?>" target="_top">Send Mail to Self</a>
+
     <div> You are majoring in <?php echo $_POST["major"] ?> </div>
     <div>You said "<?php echo $_POST["comments"] ?>"</div>
 
     <div>
-        <p>You have visited: 
-    <?php
-    $places_array = $_POST["places"];
+        <p>You have visited:</p>
+        <ul>
+            <?php
+            foreach ($_POST['places'] as $place) {
+                echo "<l$place ";
+            }
+            ?>
 
-    for($i = 0; $i < count($places_array); $i++) {
-        echo $places_array[$i];
-        echo ", ";
-    }
-    ?>
-    </p>
-    <a href=""></
-</div>
+        <a href="form.html">Back To Form</a>
+    </div>
 </body>
+
 </html>
