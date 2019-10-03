6bb04dae372e5bbb93d4f86d4f52e9c3cae9f97b
John Reiley <supermormonbrother@gmail.com>
2019-10-03 17:52:40 -0600

    more assignment 03

--- web/week03/personal/php/components/generate-items.php
+++ web/week03/personal/php/components/generate-items.php
@@ -1,4 +1,85 @@
 <?php
+$shopItemsJSON = "[
+    {
+        'id': '001',
+        'name': 'Green Chair',
+        'description': 'A solid plastic green chair that does nothing but look great in your house.',
+        'imgUrl': 'https://atlas-content-cdn.pixelsquid.com/assets_v2/194/1943517897053705554/jpeg-600/G03.jpg',
+        'price': '59.99'
+    },
+    {
+        'id': '002',
+        'name': '',
+        'description': '',
+        'imgUrl': '',
+        'price': ''
+    },
+    {
+        'id': '003',
+        'name': '',
+        'description': '',
+        'imgUrl': '',
+        'price': ''
+    },
+    {
+        'id': '004',
+        'name': '',
+        'description': '',
+        'imgUrl': '',
+        'price': ''
+    },
+    {
+        'id': '005',
+        'name': '',
+        'description': '',
+        'imgUrl': '',
+        'price': ''
+    },
+    {
+        'id': '006',
+        'name': '',
+        'description': '',
+        'imgUrl': '',
+        'price': ''
+    },
+    {
+        'id': '007',
+        'name': '',
+        'description': '',
+        'imgUrl': '',
+        'price': ''
+    },
+    {
+        'id': '008',
+        'name': '',
+        'description': '',
+        'imgUrl': '',
+        'price': ''
+    },
+    {
+        'id': '009',
+        'name': '',
+        'description': '',
+        'imgUrl': '',
+        'price': ''
+    },
+    {
+        'id': '010',
+        'name': '',
+        'description': '',
+        'imgUrl': '',
+        'price': ''
+    },
+    {
+        'id': '011',
+        'name': '',
+        'description': '',
+        'imgUrl': '',
+        'price': ''
+    }
+]";
+
+
 $shopItemsDir = "shop-items.json";
 
 if (file_exists($shopItemsDir)) {
@@ -11,6 +92,6 @@ $shopItemsFile = fopen($shopItemsDir, "r") or die("Unable to open file!");
 $shopItemsJson = fread($shopItemsFile, filesize($shopItemsDir));
 fclose($shopItemsFile);
 
-$shopItems = json_decode($shopItemsJson, true);
-print_r($shopItemsJson);
+$shopItems = json_decode($shopItemsJSON, true);
+print_r($shopItems);
 ?>
