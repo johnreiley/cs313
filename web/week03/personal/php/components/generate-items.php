01af948b4d47687987d0d6af7ec8c557ee4e4564
John Reiley <supermormonbrother@gmail.com>
2019-10-03 16:54:53 -0600

    more assignment 03

--- web/week03/personal/php/components/generate-items.php
+++ web/week03/personal/php/components/generate-items.php
@@ -1,5 +1,5 @@
 <?php
-$shopItemsDir = "shop-items.json";
+$shopItemsDir = "../../data/shop-items.json";
 $shopItemsFile = fopen($shopItemsDir, "r") or die("Unable to open file!");
 $shopItemsJson = fread($shopItemsFile, filesize($shopItemsDir));
 fclose($shopItemsFile);
