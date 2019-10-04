c4894c30104e30cc5813308c2dca971740db3fe3
John Reiley <supermormonbrother@gmail.com>
2019-10-04 15:01:35 -0600

    more assignment 03

--- web/week03/personal/php/index.php
+++ web/week03/personal/php/index.php
@@ -15,6 +15,7 @@ $thisPage = "Home";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../stylesfancy-form.css">
     <title>Shop - <?php echo $thisPage ?></title>
 </head>
 
