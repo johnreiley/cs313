568b586bea06fa8c6afff3e007b82f8d0eb18809
John Reiley <supermormonbrother@gmail.com>
2019-10-04 16:51:30 -0600

    more assignment 03

--- web/week03/personal/php/actions/add-to-cart.php
+++ web/week03/personal/php/actions/add-to-cart.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $filepath = 'https://calm-bastion-61884.herokuapp.com/week03/personal/php/index.php';
-header("Location: $filepath");
+// header("Location: $filepath");
 
 $id = $_REQUEST["id"];
 // echo($id); ///////////////////////////
