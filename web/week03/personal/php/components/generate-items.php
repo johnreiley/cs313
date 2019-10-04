d4b869b9ab82ec2ec8ba8c6569d1e4079b385ff7
John Reiley <supermormonbrother@gmail.com>
2019-10-03 22:13:07 -0600

    more assignment 03

--- web/week03/personal/php/components/generate-items.php
+++ web/week03/personal/php/components/generate-items.php
@@ -1,93 +1,6 @@
 <?php
-$shopItemsJSON = '[
-    {
-        "id": "001",
-        "name": "Green Chair",
-        "description": "A solid plastic green chair that does nothing but look great in your house.",
-        "imgUrl": "https://atlas-content-cdn.pixelsquid.com/assets_v2/194/1943517897053705554/jpeg-600/G03.jpg",
-        "price": "59.99"
-    },
-    {
-        "id": "002",
-        "name": "",
-        "description": "",
-        "imgUrl": "",
-        "price": ""
-    },
-    {
-        "id": "003",
-        "name": "",
-        "description": "",
-        "imgUrl": "",
-        "price": ""
-    },
-    {
-        "id": "004",
-        "name": "",
-        "description": "",
-        "imgUrl": "",
-        "price": ""
-    },
-    {
-        "id": "005",
-        "name": "",
-        "description": "",
-        "imgUrl": "",
-        "price": ""
-    },
-    {
-        "id": "006",
-        "name": "",
-        "description": "",
-        "imgUrl": "",
-        "price": ""
-    },
-    {
-        "id": "007",
-        "name": "",
-        "description": "",
-        "imgUrl": "",
-        "price": ""
-    },
-    {
-        "id": "008",
-        "name": "",
-        "description": "",
-        "imgUrl": "",
-        "price": ""
-    },
-    {
-        "id": "009",
-        "name": "",
-        "description": "",
-        "imgUrl": "",
-        "price": ""
-    },
-    {
-        "id": "010",
-        "name": "",
-        "description": "",
-        "imgUrl": "",
-        "price": ""
-    },
-    {
-        "id": "011",
-        "name": "",
-        "description": "",
-        "imgUrl": "",
-        "price": ""
-    }
-]';
-
 
 $shopItemsDir = __DIR__ . '/../../data/shop-items.json';
-echo $shopItemsDir;
-
-if (file_exists($shopItemsDir)) {
-    echo "The file $filename exists";
-} else {
-    echo "The file ain't nowhere";
-}
 
 $shopItemsFile = fopen($shopItemsDir, "r") or die("Unable to open file!");
 $shopItemsJson = fread($shopItemsFile, filesize($shopItemsDir));
@@ -104,5 +17,6 @@ foreach($shopItems as $item) {
             </div>
         </div>";
 }
-echo "</div>"
+echo "</div>";
+
 ?>
