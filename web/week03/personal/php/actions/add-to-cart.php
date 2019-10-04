9ab17bf9694f5b3a8f021a9edfa1c9ca176ec6fb
John Reiley <supermormonbrother@gmail.com>
2019-10-04 17:03:39 -0600

    more assignment 03

--- web/week03/personal/php/actions/add-to-cart.php
+++ web/week03/personal/php/actions/add-to-cart.php
@@ -7,7 +7,7 @@ $id = $_REQUEST["id"];
 echo($id); ///////////////////////////
 $shopItems = getShopInventory();
 print_r($shopItems);
-$itemToAdd = array_filter($shopItems, function($shopItem) use($id) { echo $shopItem->id; echo $id; return $shopItem->id == $id; })[0];
+$itemToAdd = array_filter($shopItems, function($shopItem) use($id) { echo $shopItem->id == $id; return $shopItem->id == $id; })[0];
 echo($itemToAdd);
 
 array_push($_SESSION["cart"], $itemToAdd);
