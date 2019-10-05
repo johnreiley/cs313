999260d08d14eb368e493bd0a535d934f05243dc
John Reiley <supermormonbrother@gmail.com>
2019-10-05 00:43:02 -0600

    more assignment 03

--- web/week03/personal/php/order-confirmation.php
+++ web/week03/personal/php/order-confirmation.php
@@ -28,9 +28,9 @@ $cart = $_SESSION["cart"];
     <?php include("components/navigation.php"); ?>
 
     <div class="content-container order-confirm-page">
-        <div>You're order has been comfirmed and will be shipped to the following address in 25 days:</div>
-        <div><?php echo "$fname $lname<br>$a1 $a2<br>$city, $state<br>$zipcode"; ?></div>
-        <div>Your Order:</div>
+        <div>You're order has been comfirmed and will be shipped to the following address in 25 days:</div><br>
+        <div><?php echo "$fname $lname<br>$a1 $a2<br>$city, $state<br>$zipcode"; ?></div><br>
+        <div><strong>Your Order:<strong></div>
         <div>
             <?php
             foreach ($cart as $item) {
