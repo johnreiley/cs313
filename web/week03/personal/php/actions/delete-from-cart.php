ed601abc062bac9ad2af238b912edf87efebc3d5
John Reiley <supermormonbrother@gmail.com>
2019-10-04 19:48:51 -0600

    more assignment 03

--- web/week03/personal/php/actions/delete-from-cart.php
+++ web/week03/personal/php/actions/delete-from-cart.php
@@ -1,17 +1,13 @@
 <?php
 session_start();
 $filepath = 'https://calm-bastion-61884.herokuapp.com/week03/personal/php/cart-view.php';
-// header("Location: $filepath");
+header("Location: $filepath");
 
 $id = $_REQUEST["id"];
-echo $id;
 $cartItems = $_SESSION["cart"];
-print_r($cartItems);
-
 $cartItems = array_filter($cartItems, function($cartItem) use($id) { return $cartItem->id != $id; });
-print_r($cartItems);
-$_SESSION["cart"] = [];
 
+$_SESSION["cart"] = [];
 foreach ($cartItems as $itemToAdd) {
     array_push($_SESSION["cart"], $itemToAdd);
 }
