1a25168d83e25fbe72f88f2afdf473fed5fcb1a6
John Reiley <supermormonbrother@gmail.com>
2019-10-04 19:33:27 -0600

    more assignment 03

--- web/week03/personal/php/actions/delete-from-cart.php
+++ web/week03/personal/php/actions/delete-from-cart.php
@@ -6,5 +6,10 @@ header("Location: $filepath");
 $id = $_REQUEST["id"];
 $cartItems = $_SESSION["cart"];
 
-$_SESSION["cart"] = array_filter($cartItems, function($cartItem) use($id) { return $cartItem->id != $id; });
+$cartItems = array_filter($cartItems, function($cartItem) use($id) { return $cartItem->id != $id; });
+$_SESSION["cart"] = [];
+
+foreach ($cartItems as $itemToAdd) {
+    array_push($_SESSION["cart"], $itemToAdd);
+}
 ?>
