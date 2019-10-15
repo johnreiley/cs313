964153af69bc58c1ab9a77a7ea68520f2693185c
John Reiley <supermormonbrother@gmail.com>
2019-10-15 16:20:26 -0600

    team05

--- web/week05/team/scriptureDetails.php
+++ web/week05/team/scriptureDetails.php
@@ -6,13 +6,14 @@ require 'db-connect.php';
 Scripture Details: <?php 
 $id = $_GET["id"];
 
-echo($id);
-
 $query = "SELECT book, chapter, verse, content FROM scriptures
 WHERE scripture_id = $id";
 
 foreach ($db->query($query) as $row) {
     $content = $row['content'];
+    $book = $row['book'];
+    $chapter = $row['chapter'];
+    $verse = $row['verse'];
 
     echo "<b>$book $chapter:$verse</b> - \"$content\"";
     echo "<br/>";
