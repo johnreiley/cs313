5dbf4841ed448ca1df11108504a1ed7366f41d71
John Reiley <supermormonbrother@gmail.com>
2019-10-15 16:01:38 -0600

    team05

--- web/week05/team/results.php
+++ web/week05/team/results.php
@@ -5,17 +5,17 @@ $book = $_POST["book"];
 
 echo ("book = $book");
 // $query = "
-// SELECT book, chapter, verse IN scriptures
+// SELECT book, chapter, verse FROM scriptures
 // WHERE book = $book";
 // $stmt = $db->prepare($query);
 // $stmt->execute();
 // $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($db->query("SELECT chapter, verse FROM scriptures WHERE book = $book;") as $row) {
+foreach ($db->query("SELECT chapter, verse FROM scriptures WHERE book=John;") as $row) {
     $chapter = $row['chapter'];
     $verse = $row['verse'];
     
-    echo "<a href=\"scriptureDetails.php?book=$book&chapter=$chapter&verse=$verse\">$book $chapter:$verse</a>Click Here for Details";
+    echo "<a href=\"scriptureDetails.php?book=$book&chapter=$chapter&verse=$verse\">$book $chapter:$verse</a>";
 }
 
 ?>
--- web/week05/team/scriptureDetails.php
+++ web/week05/team/scriptureDetails.php
@@ -8,16 +8,16 @@ $book = $_GET["book"];
 $chapter = $_GET["chapter"];
 $verse = $_GET["verse"];
 
-$query = 'SELECT book, chapter, verse, content IN scriptures
+$query = 'SELECT book, chapter, verse, content FROM scriptures
 WHERE book = bk=:book_name
 AND chapter = chapter=:chapter
 AND verse = verse=:verse';
 $query->execute();
 
-$select = $db->prepare('SELECT book, chapter, verse, content FROM scripture');
-$select.execute();
+// $select = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
+// $select.execute();
 
-while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
     $content = $row['content'];
 }
 
