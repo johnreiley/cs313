b3729b431c49d04c15850fb35d4c794250a1546f
John Reiley <supermormonbrother@gmail.com>
2019-10-15 16:11:44 -0600

    team05

--- web/week05/team/results.php
+++ web/week05/team/results.php
@@ -3,14 +3,6 @@ require 'db-connect.php';
 
 $book = $_POST["book"];
 
-echo ("book = $book");
-// $query = "
-// SELECT book, chapter, verse FROM scriptures
-// WHERE book = $book";
-// $stmt = $db->prepare($query);
-// $stmt->execute();
-// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 $query = "SELECT chapter, verse FROM scriptures WHERE book='$book'";
 foreach ($db->query($query) as $row) {
     $chapter = $row['chapter'];
--- web/week05/team/scriptureDetails.php
+++ web/week05/team/scriptureDetails.php
@@ -4,22 +4,23 @@ require 'db-connect.php';
 
 
 Scripture Details: <?php 
-$book = $_GET["book"];
-$chapter = $_GET["chapter"];
-$verse = $_GET["verse"];
+$book = $_POST["book"];
+$chapter = $_POST["chapter"];
+$verse = $_POST["verse"];
 
-$query = 'SELECT book, chapter, verse, content FROM scriptures
-WHERE book = bk=:book_name
-AND chapter = chapter=:chapter
-AND verse = verse=:verse';
+$query = "SELECT book, chapter, verse, content FROM scriptures
+WHERE book = '$book'
+AND chapter = '$chapter'
+AND verse = '$verse'";
 $query->execute();
 
-// $select = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
-// $select.execute();
-
-while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+foreach ($db->query($query) as $row) {
     $content = $row['content'];
+
+    echo "<b>$book $chapter:$verse</b> - \"$content\"";
+    echo "<br/>";
+    echo "<br/>";
 }
 
-echo "$book $chapter:$verse - $content";
+//echo "$book $chapter:$verse - $content";
 ?>
