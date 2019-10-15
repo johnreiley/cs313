9f4f7d0f4875c9f65ae7bad02336445dce304df9
John Reiley <supermormonbrother@gmail.com>
2019-10-15 15:53:13 -0600

    team05

--- web/week05/team/results.php
+++ web/week05/team/results.php
@@ -5,20 +5,20 @@ $book = $_POST["book"];
 
 echo ("book = $book");
 
-// $query = '
-// SELECT book, chapter, verse IN scriptures
-// WHERE book = book=:book_name';
-// $stmt = $db->prepare($query);
-// $stmt->execute(array( ':book_name' => $book));
-// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$query = "
+SELECT book, chapter, verse IN scriptures
+WHERE book = $book";
+$stmt = $db->prepare($query);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// foreach ($rows as $row) {
-//     $book = $row['book'];
-//     $chapter = $row['chapter'];
-//     $verse = $row['verse'];
+foreach ($rows as $row) {
+    $book = $row['book'];
+    $chapter = $row['chapter'];
+    $verse = $row['verse'];
     
-//     echo "<a href=\"scriptureDetails.php?book=$book&chapter=$chapter&verse=$verse\">$book $chapter:$verse</a>Click Here for Details";
-// }
+    echo "<a href=\"scriptureDetails.php?book=$book&chapter=$chapter&verse=$verse\">$book $chapter:$verse</a>Click Here for Details";
+}
 
 ?>
 
