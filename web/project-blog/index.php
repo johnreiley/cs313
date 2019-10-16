8f211b85a38961f7f2de09f463fa8ab2855ebcda
John Reiley <supermormonbrother@gmail.com>
2019-10-15 18:31:16 -0600

    initial db test

--- web/project-blog/index.php
+++ web/project-blog/index.php
@@ -19,12 +19,8 @@ $page = "Home";
     <h1><?php echo $page ?></h1>
 
     <?php
-    $query = 'SELECT post_date, post_title, post_text FROM posts';
-    $stmt = $db->prepare($query);
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    foreach ($rows as $row) {
-        echo $row['post_date'];
+    foreach ($db->query('SELECT post_date, post_title, post_text FROM posts') as $row) {
+        echo $row['post_title'] . "<br>";
     }
     // foreach (getAllPosts($db) as $post) {
     //     echo "<a>" . $post['post_title'] . "</a><br>";
