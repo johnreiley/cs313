02a9597c95cb7145d6428d43768809bed6169052
John Reiley <supermormonbrother@gmail.com>
2019-10-15 18:15:17 -0600

    initial db test

--- web/project-blog/index.php
+++ web/project-blog/index.php
@@ -1,5 +1,5 @@
 <?php
-// require 'db-queries.php';
+require 'db-queries.php';
 require 'connect-db.php';
 $db = get_db();
 $page = "Home";
@@ -12,16 +12,16 @@ $page = "Home";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog - <?php $page ?></title>
+    <title>Blog - <?php echo $page ?></title>
 </head>
 
 <body>
-    <h1><?php $page ?></h1>
+    <h1><?php echo $page ?></h1>
 
     <?php
-    // foreach (getAllPosts($db) as $post) {
-    //     echo "<a>" . $post['post_title'] . "</a><br>";
-    // }
+    foreach (getAllPosts($db) as $post) {
+        echo "<a>" . $post['post_title'] . "</a><br>";
+    }
     ?>
 
 </body>
