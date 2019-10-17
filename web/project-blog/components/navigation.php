cee11963e020e35d0bf38a6331675af1cc751fd6
John Reiley <supermormonbrother@gmail.com>
2019-10-16 22:20:58 -0600

    added more

---
+++ web/project-blog/components/navigation.php
@@ -0,0 +1,7 @@
+<header>
+    <nav>
+        <a href="index.php" class="nav-item">Home</a>
+        <a href="about.php" class="nav-item">About</a>
+        <a href="archive.php" class="nav-item">Archive</a>
+    </nav>
+</header>
