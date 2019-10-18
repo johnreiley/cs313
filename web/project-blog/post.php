5a49dce494c6d4b2bb4bd9a14ce267c85414a0f6
John Reiley <supermormonbrother@gmail.com>
2019-10-17 20:04:18 -0600

    added more

--- web/project-blog/post.php
+++ web/project-blog/post.php
@@ -26,7 +26,7 @@ $date = $post['post_date'];
     <?php require 'components/navigation.php' ?>
 
     <main>
-        <div class="post-content">
+        <div class="content-container">
             <h2 class="post-title"><?php echo $title ?></h2>
             <div class="post-info"><?php echo "$date - $author"; ?></div>
             <div class="post-body"><?php echo $post['post_text'] ?></div>
