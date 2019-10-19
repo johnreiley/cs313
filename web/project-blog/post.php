a27ebed6f4f9c4d832b60190c75f372140187690
John Reiley <supermormonbrother@gmail.com>
2019-10-19 09:59:11 -0600

    added more

--- web/project-blog/post.php
+++ web/project-blog/post.php
@@ -46,7 +46,16 @@ $date = $post['post_date'];
                        <div class=\"comment-body\">$text</div>
                     </div>
                     <div class=\"children\">";
-                    generateChildrenComments($db, $commentId);
+                    foreach (getSecondLevelComments($db, $commentId) as $comment) {
+                        $commentId = $comment['comment_id'];
+                        $user = $comment['first_name'] . " " . $comment['last_name'];
+                        $date = $comment['comment_time'];
+                        $text = $comment['comment_text'];
+                        echo "
+                        <div id=\"comment-$commentId\" class=\"comment\">
+                            <div class=\"comment-details\">$user - $date</div>
+                            <div class=\"comment-body\">$text</div>
+                        </div>";
                     echo "</div>";
                 }
                 ?>
