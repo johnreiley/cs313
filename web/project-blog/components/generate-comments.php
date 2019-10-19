2f80fc32bfa799a900c1d66463cbfcde61f9312c
John Reiley <supermormonbrother@gmail.com>
2019-10-19 09:37:10 -0600

    added more

--- web/project-blog/components/generate-comments.php
+++ web/project-blog/components/generate-comments.php
@@ -3,16 +3,20 @@
 function generateChildrenComments($db, $id)
 {
     $comments = getSecondLevelComments($db, $id);
-    foreach ($comments as $comment) {
-        $commentId = $comment['comment_id'];
-        $sUser = $comment['first_name'] . " " . $comment['last_name'];
-        $sDate = $comment['comment_time'];
-        $sText = $comment['comment_text'];
-        echo "
-        <div class=\"comment\">
-            <div class=\"comment-details\">$sUser - $sDate</div>
-            <div class=\"comment-body\">$sText</div>
-        </div>";
-        // generateChildrenComments($db, $commentId);
+    if (sizeof($comments) > 0) {
+        foreach ($comments as $comment) {
+            $commentId = $comment['comment_id'];
+            $sUser = $comment['first_name'] . " " . $comment['last_name'];
+            $sDate = $comment['comment_time'];
+            $sText = $comment['comment_text'];
+            echo "
+            <div class=\"comment\">
+                <div class=\"comment-details\">$sUser - $sDate</div>
+                <div class=\"comment-body\">$sText</div>
+            </div>";
+            generateChildrenComments($db, $commentId);
+        }
+    } else {
+        return;
     }
 }
