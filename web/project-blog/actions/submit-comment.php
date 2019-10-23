f465960655e755116bf809d4f19934b255a8d640
John Reiley <supermormonbrother@gmail.com>
2019-10-22 22:11:43 -0600

    form validation

--- web/project-blog/actions/submit-comment.php
+++ web/project-blog/actions/submit-comment.php
@@ -1,14 +1,12 @@
 <?php
-// require_once 'utilities/db-queries.php';
-// require_once 'utilities/connect-db.php';
-// $db = get_db();
+require 'utilities/db-queries.php';
+require 'utilities/connect-db.php';
+$db = get_db();
 
 $id = $_GET['id'];
 $name = $_POST['name'];
 $email = $_POST['email'];
 $comment = $_POST['comment'];
-
-// echo "$id <br> $name <br> $email <br> $comment";
 
 postNewComment($db, $id, $name, $email, $comment);
 
