731b04a4ae8d19e01d87de5c7279a9c27d38b092
John Reiley <supermormonbrother@gmail.com>
2019-10-23 22:06:45 -0600

    user login test

--- web/project-blog/admin-login.php
+++ web/project-blog/admin-login.php
@@ -10,27 +10,26 @@
 </head>
 
 <body>
-    <main>
-        <div class="content-container login-container">
-            <form class="fancy-form login-form" method="post" action="actions/sign-in.php">
-                <h2>Admin Portal</h2>
-                <div class="break"></div>
-                <div class="fancy-input">
-                    <input type="username" id="name" name="name" required>
-                    <div class="fancy-input-txt">username</div>
-                </div>
-                <div class="break"></div>
-                <div class="fancy-input">
-                    <input type="password" id="email" name="email" required>
-                    <div class="fancy-input-txt">password</div>
-                </div>
-                <div class="break"></div>
-                <div class="fancy-btn">
-                    <input type="submit" value="Login">
-                </div>
-            </form>
-        </div>
-    </main>
+
+    <div class="content-container login-container">
+        <form class="fancy-form login-form" method="post" action="actions/sign-in.php">
+            <h2>Admin Portal</h2>
+            <div class="break"></div>
+            <div class="fancy-input">
+                <input type="username" id="name" name="name" required>
+                <div class="fancy-input-txt">username</div>
+            </div>
+            <div class="break"></div>
+            <div class="fancy-input">
+                <input type="password" id="email" name="email" required>
+                <div class="fancy-input-txt">password</div>
+            </div>
+            <div class="break"></div>
+            <div class="fancy-btn">
+                <input type="submit" value="Login">
+            </div>
+        </form>
+    </div>
     <?php require 'components/footer.php' ?>
 </body>
 
