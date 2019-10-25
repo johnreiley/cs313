b0be217c6778adac40b533f1cd9a620c94f4d7de
John Reiley <supermormonbrother@gmail.com>
2019-10-25 16:21:49 -0600

    edit post test

--- web/project-blog/components/navigation.php
+++ web/project-blog/components/navigation.php
@@ -1,11 +1,14 @@
 <header>
     <nav>
-        <a href="index.php" class="nav-item">Home</a>
-        <a href="about.php" class="nav-item">About</a>
-        <a href="archive.php" class="nav-item">Archive</a>
+        <a href="index.php" class="nav-item"><i class="material-icons">home</i></a>
+        <a href="archive.php" class="nav-item"><i class="material-icons">list</i></a>
+        <a href="about.php" class="nav-item"><i class="material-icons">info</i></a>
         <?php
         if ($isAdmin)
-            echo "<a href=\"actions/sign-out.php\" class=\"nav-item sign-out-btn\">Sign out</a>";
+            echo "
+            <a href=\"actions/sign-out.php\" class=\"nav-item sign-out-btn\">
+                <i class=\"material-icons\">exit_to_app</i>
+            </a>";
         ?>
     </nav>
 </header>
