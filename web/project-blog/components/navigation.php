305450c4723e3435ee5d34a99a84378714e4c8b2
John Reiley <supermormonbrother@gmail.com>
2019-10-25 15:08:25 -0600

    edit post test

--- web/project-blog/components/navigation.php
+++ web/project-blog/components/navigation.php
@@ -3,6 +3,6 @@
         <a href="index.php" class="nav-item">Home</a>
         <a href="about.php" class="nav-item">About</a>
         <a href="archive.php" class="nav-item">Archive</a>
-        <a href="actions/signout.php" class="nav-item sign-out-btn">Sign out</a>
+        <div><a href="actions/signout.php" class="nav-item sign-out-btn">Sign out</a></div>
     </nav>
 </header>
