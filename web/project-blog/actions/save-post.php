a96c5d7a89a0a04e00f404f9f757e5d86e61555d
John Reiley <supermormonbrother@gmail.com>
2019-10-25 14:06:16 -0600

    edit post test

--- web/project-blog/actions/save-post.php
+++ web/project-blog/actions/save-post.php
@@ -1,14 +1,17 @@
 <?php
 require '../components/inject-requires-actions.php';
 
-// $title = $_POST['title'];
-// $img = $_POST['img-url'];
-// $text = $_POST['body-text'];
+$title = $_POST['title'];
+$img = $_POST['img-url'];
+$text = $_POST['body-text'];
 
-// if (isset($_POST['id'])) {
-//     updateBlogPost($db, $_POST['id'], $title, $img, $text);
-// } else {
-//     postNewBlogPost($db, $title, $img, $text);
-// }
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    updateBlogPost($db, $id, $title, $img, $text);
+    header("Location: ../post.php?id=$id");
+} else {
+    postNewBlogPost($db, $title, $img, $text);
+    header("Location: ../index.php");
+}
 
 ?>
