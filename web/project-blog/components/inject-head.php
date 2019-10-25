95a696c19d46dd1a0865bda982ce47db04f3d0ba
John Reiley <supermormonbrother@gmail.com>
2019-10-25 13:54:38 -0600

    edit post test

--- web/project-blog/components/inject-head.php
+++ web/project-blog/components/inject-head.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="css/animations.css">
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/fancy-form.css">
 <script src="js/fancy-form.js" defer></script>
