3f22ebc5c5e8e7a3f8e5e152214659bd5bdea82f
John Reiley <supermormonbrother@gmail.com>
2019-10-25 16:25:38 -0600

    nav stuff

--- web/project-blog/components/navigation.php
+++ web/project-blog/components/navigation.php
@@ -1,8 +1,8 @@
 <header>
     <nav>
-        <a href="index.php" class="nav-item"><i class="material-icons">home</i></a>
-        <a href="archive.php" class="nav-item"><i class="material-icons">list</i></a>
-        <a href="about.php" class="nav-item"><i class="material-icons">info</i></a>
+        <a href="index.php" class="nav-item home-btn"><i class="material-icons">home</i></a>
+        <a href="archive.php" class="nav-item">All Posts</a>
+        <a href="about.php" class="nav-item">About</a>
         <?php
         if ($isAdmin)
             echo "
