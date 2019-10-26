4df7af7557fe2ea6e8db8aafcaf327a90cd02d7d
John Reiley <supermormonbrother@gmail.com>
2019-10-25 20:19:08 -0600

    debugging post loading

--- web/project-blog/actions/delete-post.php
+++ web/project-blog/actions/delete-post.php
@@ -1,6 +1,11 @@
 <?php
+require '../components/inject-requires.php';
 
+$id = $_POST['post-delete-id'];
 
-
+if ($isAdmin) {
+    deleteBlogPost($db, $id);
+}
+header('Location: ../index.php');
 
 ?>
