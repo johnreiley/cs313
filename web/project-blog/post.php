b60fbf2589bba583d2d38f7a0b85462b824c7ab9
John Reiley <supermormonbrother@gmail.com>
2019-10-25 21:04:39 -0600

    debugging post loading

--- web/project-blog/post.php
+++ web/project-blog/post.php
@@ -6,7 +6,7 @@ $id = $_GET['id'];
 $post = getSinglePost($db, $id);
 $title = $post['post_title'];
 $date = $post['post_date'];
-$date = date("F jS, Y", strtotime(explode(" ", $date)[0]));
+$date = date("F j, Y", strtotime(explode(" ", $date)[0]));
 $img = $post['post_img'];
 ?>
 
