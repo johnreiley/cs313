08a611ea390a5b3873fa4743c0849584695f5fd3
John Reiley <supermormonbrother@gmail.com>
2019-10-25 18:35:43 -0600

    nav stuff

--- web/project-blog/components/inject-head.php
+++ web/project-blog/components/inject-head.php
@@ -1,5 +1,5 @@
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="css/animations.css">
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/fancy-form.css">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <script src="js/fancy-form.js" defer></script>
