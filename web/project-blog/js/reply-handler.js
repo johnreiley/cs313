8f84601b4b1099ae0e4f91c2bdf18c8ca85391cf
John Reiley <supermormonbrother@gmail.com>
2019-10-26 22:34:24 -0600

    comment reply

--- web/project-blog/js/reply-handler.js
+++ web/project-blog/js/reply-handler.js
@@ -1,7 +1,7 @@
 function openCommentBlock(commentId) {
     let comment = document.querySelector(`#comment-${commentId}`);
-    let name = document.querySelector(`#comment-${commentId} .comment-details`).innerText;
-    console.log(name);
+    let name = document.querySelector(`#comment-${commentId} .comment-details`).innerText.split(" -")[0];
+    
 }
 
 
