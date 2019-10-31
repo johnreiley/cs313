960f03feec61dbecf5f18e496c346eb1d420a731
John Reiley <supermormonbrother@gmail.com>
2019-10-31 15:26:34 -0600

    quilljs

--- web/project-blog/draft-post.php
+++ web/project-blog/draft-post.php
@@ -29,6 +29,15 @@ if (isset($_POST['post-edit-id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Main Quill library -->
+    <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
+    <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
+
+    <!-- Theme included stylesheets -->
+    <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+    <link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet">
+
     <?php require 'components/inject-head.php' ?>
     <script src="js/post-draft-previewer.js" defer></script>
     <title>Blog - Draft Post</title>
@@ -57,6 +66,13 @@ if (isset($_POST['post-edit-id'])) {
                     <textarea name="body-text" id="body-text" cols="30" rows="10"><?php echo $text ?></textarea>
                     <div class="fancy-input-txt">body text</div>
                 </div>
+                
+                <script>
+                    var quill = new Quill('#editor', {
+                        theme: 'snow'
+                    });
+                </script>
+
                 <div class="break"></div>
                 <div class="fancy-btn">
                     <input type="submit" id="save-post-btn" value="Save">
