eacc89474dc5e15986e8a74cc1354cf11d326d2a
John Reiley <supermormonbrother@gmail.com>
2019-10-31 15:30:40 -0600

    quilljs

--- web/project-blog/draft-post.php
+++ web/project-blog/draft-post.php
@@ -30,10 +30,6 @@ if (isset($_POST['post-edit-id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <!-- Main Quill library -->
-    <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
-    <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
-
     <!-- Theme included stylesheets -->
     <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
     <link href="//cdn.quilljs.com/1.3.6/quill.bubble.css" rel="stylesheet">
@@ -92,6 +88,9 @@ if (isset($_POST['post-edit-id'])) {
     <?php require 'components/footer.php' ?>
 
 </body>
+
+<!-- Main Quill library -->
+<script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
 <script>
     document.querySelector('#save-post-btn').onclick = () => {
         window.onbeforeunload = undefined;
