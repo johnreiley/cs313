44f7082f43560f7590efa93564733a3ae118f636
John Reiley <supermormonbrother@gmail.com>
2019-10-31 21:50:25 -0600

    quilljs

--- web/project-blog/js/post-draft-previewer.js
+++ web/project-blog/js/post-draft-previewer.js
@@ -1,6 +1,7 @@
 var title = document.querySelector('#title');
 var imgUrl = document.querySelector('#img-url');
 var bodyText = document.querySelector('#body-text');
+var editor = document.querySelector('.ql-editor');
 
 var titleDiv = document.querySelector('.page-title');
 var info = document.querySelector('.post-info');
@@ -10,19 +11,20 @@ var bodyTextDiv = document.querySelector('.post-body');
 
 titleDiv.innerText = title.value;
 img.src = imgUrl.value;
-bodyTextDiv.innerHTML = bodyText.value;
+bodyTextDiv.innerHTML = editor.innerHTML;
 
 title.addEventListener('keyup', keyupListener);
 imgUrl.addEventListener('keyup', keyupListener);
-bodyText.addEventListener('keyup', keyupListener);
+editor.addEventListener('keyup', keyupListener);
 
 title.addEventListener('blur', keyupListener);
 imgUrl.addEventListener('blur', keyupListener);
-bodyText.addEventListener('blur', keyupListener);
+editor.addEventListener('blur', keyupListener);
 
 function keyupListener(e) {
     titleDiv.innerText = title.value;
-    // info.innerText = Date.now()
     img.src = imgUrl.value;
-    bodyTextDiv.innerHTML = bodyText.value;
+    bodyTextDiv.innerHTML = editor.innerHTML;
+
+    bodyText.value = editor.innerHTML;
 }
