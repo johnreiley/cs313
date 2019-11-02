b1ff02a777eeabb12ee2cd4bd812b4e0fb8e92f3
John Reiley <supermormonbrother@gmail.com>
2019-11-02 14:39:52 -0600

    fixed deleting a post that has comments

--- web/project-blog/actions/delete-post.php
+++ web/project-blog/actions/delete-post.php
@@ -1,11 +1,11 @@
 <?php
 require '../components/inject-requires-actions.php';
 
-$id = $_POST['post-delete-id'];
+// $id = $_POST['post-delete-id'];
 
-if ($isAdmin) {
-    deleteBlogPost($db, $id);
-}
+// if ($isAdmin) {
+//     deleteBlogPost($db, $id);
+// }
 header('Location: ../index.php');
 
 ?>
